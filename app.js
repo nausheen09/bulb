48c0ee1ba338cec5e93cbf06c405150307e668fc
nausheen09 <nausheennadeem849@gmail.com>
2024-08-06 20:54:19 +0500

    task

---
+++ app.js
@@ -0,0 +1,8 @@
+var img = document.getElementById("open");
+function onn(){
+    img.src = "images 2.png"
+    document.getElementById("offf").disabled = false
+}
+function off(){
+    img.src ="images 1.png"
+}
